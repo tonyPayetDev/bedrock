f2f63d2e6a097d6db5a61d9ed6bbec42b05a0200
tony <tony.payet.professionnel@gmail.com>
2021-09-02 17:21:18 +0400

    Update api.php

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
@@ -185,7 +185,7 @@ function biens(WP_REST_Request $request)
         foreach ($meta as $key => $value_meta) {
             $tab["id"]=$value->ID;
             $tab["post_name"]=$value->post_name;
-            $url = wp_get_attachment_image_src($value->photo, 'medium-large')[0];// recupere juste l'ul
+            $url = wp_get_attachment_image_src($value->photo, 'medium')[0];// recupere juste l'ul
             
             $tab["photo"]= $url;
             
