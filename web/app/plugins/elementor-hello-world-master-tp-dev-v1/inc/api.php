f6c725fc4ad1cab1c512139dc2656de65d3f545f
tony <tony.payet.professionnel@gmail.com>
2021-09-22 17:08:10 +0400

    Update api.php
    add tableau map sur retour pour l'affichage google maps

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
@@ -194,8 +194,35 @@ function biens(WP_REST_Request $request)
         }
         $tab_meta['data'][]=$tab;
     }
-   
- 
+    // pour la maps
+    $request_p =  array(
+        'post_type' => $type,
+        'posts_per_page'   => -1,//-1 all
+        'meta_query' => $r,
+        'paged' => $paged,
+        'orderby' => 'date_saisie',
+        'meta_type' => 'DATE',
+        'order' => 'DESC'
+      ) ;
+    $biens = new WP_query($request_p);
+    $ville ;
+    foreach ($biens->posts as $key => $value) {
+        $meta = get_post_meta($value->ID);
+        $tab=[];
+        foreach ($meta as $key => $value_meta) {
+            $tab["id"]=$value->ID;
+            $tab["post_name"]=$value->post_name;
+            $url = wp_get_attachment_image_src($value->photo, 'full')[0];// recupere juste l'ul
+            
+            $tab["photo"]= $url;
+            $tab[$key]=$value_meta[0];
+
+            if ($key=="ville") {
+                $ville=$value_meta[0];
+            }
+        }
+        $tab_meta['map'][$ville][]=$tab;
+    }
     $request_nb =  array(
         'post_type' => $type,
         'posts_per_page'   => -1 ,
