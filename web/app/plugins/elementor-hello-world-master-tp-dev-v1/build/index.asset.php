23b29d4fa0db031cf64a18ad172b566e710fdba0
tony <tony.payet.professionnel@gmail.com>
2021-10-18 10:04:22 +0400

    Update build

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/build/index.asset.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '76d955bc44018ca3db2b425a037fdcc7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-polyfill'), 'version' => '7652555f391b6794aa00041dd98579e5');
