1ffd6757c70efcff33c31d41e4d62b36ea438f90
tony <tony.payet.professionnel@gmail.com>
2021-10-27 17:48:34 +0400

    Update custom_shorcode.php

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
@@ -106,13 +106,15 @@ function cux_gallery_embed_shortcode($atts)
 
 <div id="slider" class="flexslider">
     <ul class="slides">
-        <?php foreach ($images as $image_id): ?>
-
-        <li>
-            <img style=" border-color:<?php echo $atts['color']; ?>;margin-left: 0px;margin-top: 0px;background: rgb(250, 250, 250);border-width: 0px 0px 3px;border-style: solid;border-image: initial;border-radius: 0px 23px 23px 0px;padding: 0px;width: 879px;height: 530px;box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;left: 5px;"
-                src="<?php echo wp_get_attachment_image($image_id, "full"); ?>" />
-        </li>
-        <?php endforeach; ?>
+        <?php   if ($images): ?>
+        <ul>
+            <?php foreach ($images as $image_id): ?>
+            <li>
+                <?php echo wp_get_attachment_image($image_id, 'full'); ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+        <?php endif; ?>
     </ul>
 </div>
 <div class="custom-navigation">
