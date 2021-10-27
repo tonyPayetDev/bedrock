8f04dc587f050a747b52bb491ac4b029c4f2edd6
tony <tony.payet.professionnel@gmail.com>
2021-10-27 17:41:21 +0400

    Udpate cptui

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/cptui.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/cptui.php
@@ -448,7 +448,7 @@ acf_add_local_field_group(array(
                 'class' => '',
                 'id' => '',
             ),
-            'return_format' => 'array',
+            'return_format' => 'url',
             'preview_size' => 'medium',
             'insert' => 'append',
             'library' => 'all',
