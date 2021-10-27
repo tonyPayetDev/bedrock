ce02829a4189989532f274f63aca54f8b3dc81cc
tony <tony.payet.professionnel@gmail.com>
2021-10-27 17:00:02 +0400

    Update custom_shorcode.php

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
@@ -108,10 +108,10 @@ function cux_gallery_embed_shortcode($atts)
     <ul class="slides">
         <?php foreach ($images as $image): $url=wp_get_attachment_image($image, "full"); ?>
         <li>
-            <img src=<?php echo $url; ?> style="border-color:<?php echo $atts['color']; ?>;margin-left: 0px;margin-top: 0px;background: rgb(250,
-                250, 250);border-width: 0px 0px 3px;border-style: solid;border-image: initial;border-radius: 0px 23px
-                23px 0px;padding: 0px;width: 879px;height: 530px;box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0,
-                0, 0, 0.19) 0px 6px 20px 0px;left: 5px;" alt="" />
+            <img src=<?php echo $url ; ?> style='border-color:<?php echo $atts['color']; ?>;margin-left: 0px;margin-top:
+                0px;background: rgb(250, 250, 250);border-width: 0px 0px 3px;border-style: solid;border-image:
+                initial;border-radius: 0px 23px 23px 0px;padding: 0px;width: 879px;height: 530px;box-shadow: rgba(0, 0,
+                0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;left: 5px;'></img>
         </li>
         <?php endforeach; ?>
     </ul>
