7374ebc53c89de6df8c2423c97efd60511a7d3e4
tony <tony.payet.professionnel@gmail.com>
2021-10-27 16:43:48 +0400

    Update custom_shorcode.php

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/custom_shorcode.php
@@ -85,7 +85,7 @@ function cux_gallery_embed_shortcode($atts)
         foreach (json_decode($images) as $image): ?>
         <li>
             <img style=" border-color:<?php echo $atts['color']; ?>;margin-left: 0px;margin-top: 0px;background: rgb(250, 250, 250);border-width: 0px 0px 3px;border-style: solid;border-image: initial;border-radius: 0px 23px 23px 0px;padding: 0px;width: 879px;height: 530px;box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;left: 5px;"
-                src="<?php echo $image->url; ?>" />
+                src="<?php echo $image->url; ?>"></img>
         </li>
         <?php endforeach; ?>
 
@@ -109,7 +109,7 @@ function cux_gallery_embed_shortcode($atts)
         <?php foreach ($images as $image): ?>
         <li>
             <img style=" border-color:<?php echo $atts['color']; ?>;margin-left: 0px;margin-top: 0px;background: rgb(250, 250, 250);border-width: 0px 0px 3px;border-style: solid;border-image: initial;border-radius: 0px 23px 23px 0px;padding: 0px;width: 879px;height: 530px;box-shadow: rgba(0, 0, 0, 0.2) 0px 4px 8px 0px, rgba(0, 0, 0, 0.19) 0px 6px 20px 0px;left: 5px;"
-                src=<?php echo wp_get_attachment_image($image, "full") ?> />
+                src=<?php echo wp_get_attachment_image($image, "full") ?>></img>
         </li>
         <?php endforeach; ?>
     </ul>
