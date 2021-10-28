3b334e6b4583bcae46f96e0017f6238025bcdcfd
tony <tony.payet.professionnel@gmail.com>
2021-10-28 12:35:50 +0400

    update cache

--- web/wp-config.php
+++ web/wp-config.php
@@ -5,6 +5,7 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
+define( 'WPCACHEHOME', 'C:\wamp64\www\bedrock\web\app\plugins\wp-super-cache/' );
 define('WP_CACHE', true);
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
