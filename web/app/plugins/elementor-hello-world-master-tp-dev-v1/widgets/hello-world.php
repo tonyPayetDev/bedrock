564e0a854004049796c4b1d352271290e77fdfeb
tony <tony.payet.professionnel@gmail.com>
2021-10-29 14:55:29 +0400

    correctif pour rapidité site
    fais la requete juste si l'utilisateur est connecté evite de charger sur la premier page

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/widgets/hello-world.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/widgets/hello-world.php
@@ -1383,22 +1383,25 @@ class Hello_World extends Widget_Base
     public function get_name_select($opt_key)
     {
         $tab_key_post= [];
-        $page_settings_manager = \Elementor\Core\Settings\Manager::get_settings_managers('page');
-        $page_settings_model = $page_settings_manager->get_model(get_the_ID());
-        $url =$page_settings_model->get_settings($opt_key);
-        $response = wp_remote_get($url."&name_select=true");//todo a voir pour optimiser la recuperation des champs
-        $body     = wp_remote_retrieve_body($response);
-        $body =json_decode($body);
-        if ($body && $opt_key) {
-            foreach ($body as $key => $value) {
-                if ($key=='name_select') {
-                    foreach ($value as $key2 => $value2) {
-                        $tab_key_post[$value2 ]=  esc_html__($value2, 'elementskit-lite');
+        
+        if (get_current_user_id()) {
+            $page_settings_manager = \Elementor\Core\Settings\Manager::get_settings_managers('page');
+            $page_settings_model = $page_settings_manager->get_model(get_the_ID());
+            $url =$page_settings_model->get_settings($opt_key);
+            $response = wp_remote_get($url."&name_select=true");//todo a voir pour optimiser la recuperation des champs
+            $body     = wp_remote_retrieve_body($response);
+            $body =json_decode($body);
+        
+            if ($body && $opt_key) {
+                foreach ($body as $key => $value) {
+                    if ($key=='name_select') {
+                        foreach ($value as $key2 => $value2) {
+                            $tab_key_post[$value2 ]=  esc_html__($value2, 'elementskit-lite');
+                        }
                     }
                 }
             }
         }
-
         return $tab_key_post;
     }
     
@@ -1406,8 +1409,10 @@ class Hello_World extends Widget_Base
     {
         $settings = $this->get_settings_for_display();
         $page_settings_manager = \Elementor\Core\Settings\Manager::get_settings_managers('page');
+
         $page_settings_model = $page_settings_manager->get_model(get_the_ID());
         $url =$page_settings_model->get_settings($settings['view']);
+        
         $cpt="";
         // fin dynamique des champs todo mette dans une fonction
         $file = dirname(__DIR__)."/inc/jsonFile.json";
