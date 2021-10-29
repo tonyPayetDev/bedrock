ec61007ba09a2e8e2273962cd6a98c6946ba9d4f
tony <tony.payet.professionnel@gmail.com>
2021-10-29 15:07:28 +0400

    Update hello-world.php

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/widgets/hello-world.php
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/widgets/hello-world.php
@@ -1384,20 +1384,18 @@ class Hello_World extends Widget_Base
     {
         $tab_key_post= [];
         
-        if (get_current_user_id()) {
-            $page_settings_manager = \Elementor\Core\Settings\Manager::get_settings_managers('page');
-            $page_settings_model = $page_settings_manager->get_model(get_the_ID());
-            $url =$page_settings_model->get_settings($opt_key);
-            $response = wp_remote_get($url."&name_select=true");//todo a voir pour optimiser la recuperation des champs
-            $body     = wp_remote_retrieve_body($response);
-            $body =json_decode($body);
-        
-            if ($body && $opt_key) {
-                foreach ($body as $key => $value) {
-                    if ($key=='name_select') {
-                        foreach ($value as $key2 => $value2) {
-                            $tab_key_post[$value2 ]=  esc_html__($value2, 'elementskit-lite');
-                        }
+        $page_settings_manager = \Elementor\Core\Settings\Manager::get_settings_managers('page');
+        $page_settings_model = $page_settings_manager->get_model(get_the_ID());
+        $url =$page_settings_model->get_settings($opt_key);
+        $response = wp_remote_get($url."&name_select=true");//todo a voir pour optimiser la recuperation des champs
+        $body     = wp_remote_retrieve_body($response);
+        $body =json_decode($body);
+    
+        if ($body && $opt_key) {
+            foreach ($body as $key => $value) {
+                if ($key=='name_select') {
+                    foreach ($value as $key2 => $value2) {
+                        $tab_key_post[$value2 ]=  esc_html__($value2, 'elementskit-lite');
                     }
                 }
             }
