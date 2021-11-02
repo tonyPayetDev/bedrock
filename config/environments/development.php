85019c2a1857a6efdf06c2ec497ca73f0854b5b8
tony <tony.payet.professionnel@gmail.com>
2021-11-02 11:20:18 +0400

    Update development.php
    permet l'instalation d'extension en prod

--- config/environments/development.php
+++ config/environments/development.php
@@ -16,4 +16,4 @@ Config::define('WP_MEMORY_LIMIT', '1024M');
 ini_set('display_errors', '1');
 
 // Enable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', false);
+Config::define('DISALLOW_FILE_MODS', true);
