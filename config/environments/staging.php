c3b7198a1244321085c352e9a8250ff158af3f47
tony <tony.payet.professionnel@gmail.com>
2021-11-02 12:10:27 +0400

    Update staging.php

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -13,5 +13,4 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('WP_DEBUG', true);`
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
-Config::define('WP_DEBUG_LOG', true);
 Config::define('DISALLOW_FILE_MODS', false);
