6517ab0e5039e1d3df5a3f6900de108f33ff4281
tony <tony.payet.professionnel@gmail.com>
2021-11-08 12:01:43 +0400

    bug quand on cliquer sur louer ne fais pas la bonne requete
    bug quand on cliquer sur louer ne fais pas la bonne requete
    a voir si ne creer pas un autre bug

--- web/app/plugins/elementor-hello-world-master-tp-dev-v1/src/App.js
+++ web/app/plugins/elementor-hello-world-master-tp-dev-v1/src/App.js
@@ -40,14 +40,7 @@ const App = (props) => {
       tab[data.name] = value;
     }
     tabDefault[data.name] = { name: data.name, label: value, value: value, active: true } // tableau qui remplis les valeurs par defaut lors de la rdirection vers page recherche recupere 
-    // recupere les valeurs active des filtre active
-    if (!params.first_load && data.value) {
-      data.value.map((value, index) => {
-        if (value.ekit_tab_active != false) {
-          url_construct[data.name] = value.value;
-        }
-      });
-    }
+
   });
   const style = {
     backgroundColor: params.color ? params.color : "#ffffff",
