0d1187e11472505e4f859d9ddbe8fc127c9ca84d
tony <tony.payet.professionnel@gmail.com>
2021-11-30 11:59:22 +0400

    Update staging.php

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -14,8 +14,6 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 Config::define('DISALLOW_FILE_MODS', false);
-Config::define('WP_DEBUG', true);
-Config::define('WP_DEBUG_LOG', true);
-Config::define('JETPACK_DEV_DEBUG', true);
-Config::define('WP_MEMORY_LIMIT', '1024M');
-Config::define('JETPACK_DEV_DEBUG', true);
+Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG_LOG', false);
+Config::define('WP_MEMORY_LIMIT', '512M');
