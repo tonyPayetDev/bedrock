100f2b7bc9fe170ac30dc25eedcbbe10b3cc4b87
tony <tony.payet.professionnel@gmail.com>
2022-01-19 12:10:22 +0400

    Update api.php
    correctif api qui renvoyer des valeurs  fausse dans les autres  enregistrement quand elle apparaisser

--- public_html/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
+++ public_html/app/plugins/elementor-hello-world-master-tp-dev-v1/inc/api.php
@@ -2,7 +2,7 @@
 
 
 /**
- * liste des noms des selects
+ * liste des noms des select
  *
  * @param array $data Options for the function.
  * @return string|null Post title for the latest, * or null if none.
@@ -22,138 +22,12 @@ function name_select($type)
         }
     }
           
-    $tab[]="details";
+    $tab[]="details"; // ajout des champs dans la liste
     $tab[]="id";
 
     return array_unique($tab)  ;
 }
-/**
- * liste des secteurs
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-function secteur(WP_REST_Request $request)
-{
-    $secteur =array(
-                array("value"=>"Nord","label"=>"Nord"),
-                array("value"=>"Sud","label"=>"Sud"),
-                array("value"=>"Est","label"=>"Est"),
-                array("value"=>"Ouest","label"=>"Ouest")
-            
-            );
 
-    return $secteur  ;
-}
-/**
- * liste des select pour un critére donner
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-function critere(WP_REST_Request $request)
-{
-    $criteres= $request->get_param('criteres');
-    $request_p =  array(
-      'post_type' => "programmes",
-      'posts_per_page'   => -1,//-1 all
-    ) ;
-    $biens = new WP_query($request_p);
-    foreach ($biens->posts as $key => $value) {
-        $meta = get_post_meta($value->ID);
-    
-        foreach ($meta as $key => $value_meta) {
-            if ($key==$criteres) {
-                $tab[]=$value_meta[0];
-            }
-        }
-    }
-    
-    return array_unique($tab)  ;
-}
-/**
- * liste des type de prestations
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-function type(WP_REST_Request $request)
-{
-    $type =array(
-                array("value"=>"Vente","label"=>"Acheter"),
-                array("value"=>"Location","label"=>"Louer"),
-            
-            );
-
-    return  $type  ;
-}
-
-
-/**
- * liste des type de prestations
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-function url(WP_REST_Request $request)
-{
-    $url="url_api";
-
-    $meta=     array(
-        'key' =>  'api',
-      );
-    $request_p =  array(
-        'post_type' => "page",
-
-        'meta_query' => $meta,
-        'meta_type' => 'DATE',
-        
-      ) ;
-  
-    $biens = new WP_query($request_p);
-    $tab_meta["biens" ][]="";
-    foreach ($biens->posts as $key => $value) {
-        $meta = get_post_meta($value->ID);
-        foreach ($meta as $key => $value_meta) {
-            $tab["id"]=$value->ID;
-            $tab["api"]=$value->api;
-            $tab["post_name"]=$value->post_name;
-        }
-        $tab_meta["biens" ][]=$tab;
-    }
-
-    return   $tab_meta   ;
-}
-/**
- * liste des type de prestations
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-function params(WP_REST_Request $request)
-{
-    $file = dirname(__DIR__)."/inc/jsonFile.json";
-    $data = file_get_contents($file);
-    $obj = json_decode($data);
-    //$value=false; // enlever quand passe sur bedrock
-    return $obj ;
-}
-/**
- * retourne la liste des biens
- *
- * @param array $data Options for the function.
- * @return string|null Post title for the latest, * or null if none.
- */
-
-function title_filter($where, &$wp_query)
-{
-    global $wpdb;
-    // 2. pull the custom query in here:
-    if ($search_term = $wp_query->get('search_prod_title')) {
-        $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql(like_escape($search_term)) . '%\'';
-    }
-    return $where;
-}
 
 function biens(WP_REST_Request $request)
 {
@@ -201,13 +75,13 @@ function biens(WP_REST_Request $request)
         $biens = new WP_query($request_p);
         $ville ;
         $per_page=1;
-        $tab=[];
         $cpt=0;
         
         foreach ($biens->posts as $key => $value) {
+            $tab=[];
             $meta = get_post_meta($value->ID);
 
-            if ($cpt==10) {// decoupe par paquer de 10
+            if ($cpt==10) {// decoupe par paquet de 10
                 $per_page++;
                 $cpt=0;
             }
@@ -224,7 +98,7 @@ function biens(WP_REST_Request $request)
          
                 $tab[$key]=$value_meta[0];
             }
-        
+            
             $tab_meta['data'][$per_page][]=$tab;
         }
     
