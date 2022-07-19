aeaa47a68e14375d90fb833e349b6346d41f0e48
tonyPayetDev <tony.payet.professionnel@gmail.com>
2022-07-19 22:21:30 +0400

    add elementor theme

--- config/environments/development.php
+++ config/environments/development.php
@@ -16,4 +16,4 @@ Config::define('WP_MEMORY_LIMIT', '512M');
 ini_set('display_errors', '1');
 
 // Enable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', true);
+Config::define('DISALLOW_FILE_MODS', false);
